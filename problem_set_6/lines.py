06b117d11dfc8453248b79379ba59a84bd88fc0e
munir-moh <munirmuhd120@gmail.com>
2025-11-29 23:59:17 +0100

    Implement line counting program in lines.py

---
+++ problem_set_6/lines.py
@@ -0,0 +1,29 @@
+"""Even so, in a file called lines.py, implement a program that expects exactly one command-line argument, the name (or path) of a Python file, and outputs the number of lines of code in that file, excluding comments and blank lines. 
+If the user does not specify exactly one command-line argument, or if the specified file’s name does not end in .py, or if the specified file does not exist, the program should instead exit via sys.exit.
+
+Assume that any line that starts with #, optionally preceded by whitespace, is a comment. (A docstring should not be considered a comment.) Assume that any line that only contains whitespace is blank."""
+
+import sys
+import os
+
+if len(sys.argv) != 2:
+    sys.exit()
+
+filename = sys.argv[1]
+
+if not filename.endswith(".py"):
+    sys.exit()
+
+if not os.path.isfile(filename):
+    sys.exit()
+
+count = 0
+
+with open(filename) as f:
+    for line in f:
+        stripped = line.strip()
+        if stripped == "" or stripped.startswith("#"):
+            continue
+        count += 1
+
+print(count)
